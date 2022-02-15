3907a9e44ed65a942df835a0588c89fcd167da92
NitzanSmulevici <nitzanye@gmail.com>
2022-02-15 11:20:26 +0200

    add about html

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -12,3 +12,12 @@ import heroImageMobile from "../images/background-image-mobile.png";
 import heartBallonsImage from "../images/holding-ballons-of-love-image.png";
 import heartFaceImage from "../images/heart-face-image.png";
 import heartHandsImage from "../images/share-your-love-image";
+
+const imgTag = document.querySelector(".section__image");
+imgTag.src = heartBallonsImage;
+
+// const imgTag = document.querySelector(".section__image");
+// imgTag.src = heartFaceImage;
+
+// const imgTag = document.querySelector(".section__image");
+// imgTag.src = heartHandsImage;
