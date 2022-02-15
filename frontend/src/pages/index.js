11141f8e104c8abc6e1c6d63fedec30be50e6ad7
Shraddha <mailmeatshraddha@gmail.com>
2022-02-15 00:57:51 -0500

    feat: add header and hero section

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,34 +3,59 @@
 // ********************************************************************************************* //
 // Import main stylesheet
 import "../pages/index.css";
+import "animate.css";
 
-// Import main stylesheet
-
-// Import any images
+// Import images
 import logo from "../images/logo.svg";
-import logo-mobile from "../images/logo-mobile.svg";
-import background-image from "../images/background-image.png";
-import background-image-mobile from "../images/background-image-mobile.png";
+import logoMobile from "../images/logo-mobile.svg";
+import heroBgImage from "../images/background-image.png";
+import heroBgImageMobile from "../images/background-image-mobile.png";
 
-
+// Import modules
 import Api from "../components/Api.js";
+
+// Import constants
+import {
+  pageLogoImgElement,
+  hamburger,
+  navList,
+} from "../utils/constants.js";
+
+// ********************************************************************************************* //
+//                                        Set images                                             //
+// ********************************************************************************************* //
+
+pageLogoImgElement.src = logo;
 
 // ********************************************************************************************* //
 //                              Establish connection with API                                    //
 // ********************************************************************************************* //
-const api = new Api({
-  baseUrl: "http://localhost:3000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const api = new Api({
+//   baseUrl: "http://localhost:3000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-api
-  .getTweets()
-  .then((tweets) => {
-    const json = tweets === "" ? {} : JSON.parse(tweets);
-    console.log(json);
-  })
-  .catch((err) => console.log(err));
+// api
+//   .getTweets()
+//   .then((tweets) => {
+//     const json = tweets === "" ? {} : JSON.parse(tweets);
+//     console.log(json);
+//   })
+//   .catch((err) => console.log(err));
 
 
+// Function to toggle hamburger
+const toggleHamburger = () => {
+  hamburger.classList.toggle("hamburger_active");
+  navList.classList.toggle("nav__list_active");
+}
+
+hamburger.addEventListener("click", toggleHamburger);
+
+// Function to close the mobile menu if any of the menu link is clicked
+const closeMobileMenu = () => {
+  hamburger.classList.remove("hamburger_active");
+  navList.classList.remove("nav__list_active");
+}
